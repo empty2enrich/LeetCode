8ab12b165987a4f59e19a68d13c21615ad78a71d
emptyknowledge <2056374813@qq.com>
2017-04-06 14:26:55 +0800

    finish

--- MedianOfTwoSortedArray/MedianOfTwoSortedArray.java
+++ MedianOfTwoSortedArray/MedianOfTwoSortedArray.java
@@ -5,41 +5,41 @@ public class MedianOfTwoSortedArray {
 	int secondArrStartIndex;
 	int secondArrMiddleIndex;
 	int secondArrEndIndex;
-  int nowIndex;
+	int[] nums1;
+	int[] nums2;
+	int nowIndex;
 	int sortIndex;
 	boolean isOdd;
+  boolean isNumsLonger;
 
 	int index;//
+	public MedianOfTwoSortedArray(){};
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+      this.isNumsLonger = nums1.length>nums2.length?true:false;
+    	this.nums1 = this.isNumsLonger?nums2:nums1;
+    	this.nums2 = this.isNumsLonger?nums1:nums2;
         this.firstArrStartIndex = 0;
-        this.firstArrEndIndex = nums1.length - 1;
-        this.firstArrMiddleIndex = (this.firstArrStartIndex + this.firstArrEndIndex)/2;
+        this.firstArrEndIndex = this.nums1.length - 1;
+        this.firstArrMiddleIndex = (this.firstArrStartIndex + this.firstArrEndIndex)/2;// + (this.firstArrStartIndex + this.firstArrEndIndex)%2;
         this.secondArrStartIndex = 0;
-        this.secondArrEndIndex = nums2.length() -1;
-        this.secondArrMiddleIndex = (this.secondArrStartIndex + this.secondArrEndIndex)/2;
-        this.isOdd = (nums1.length+nums2.length)%2==0?false : true;
+        this.secondArrEndIndex = this.nums2.length -1;
+        this.secondArrMiddleIndex = (this.secondArrStartIndex + this.secondArrEndIndex)/2;// + (this.secondArrStartIndex + this.secondArrEndIndex)%2;
+        this.isOdd = (this.nums1.length+this.nums2.length)%2==0?false : true;
+
+        //如果nums1或nums2为空：
+        if(this.nums1.length == 0||this.nums2.length == 0){
+          if(isOdd)
+            return this.nums1.length==0?Double.valueOf(this.nums2[this.nums2.length/2]):Double.valueOf(nums1[nums1.length/2]);
+          else
+            return this.nums1.length==0?Double.valueOf(this.nums2[this.nums2.length/2]+this.nums2[this.nums2.length/2-1])/2:Double.valueOf(nums1[nums1.length/2]+nums1[nums1.length/2-1])/2;
+        }
         //nums1 nums2 不重叠
-       	if(nums2[secondArrEndIndex]<nums1[firstArrStartIndex]||nums2[secondArrStartIndex]>nums1[firstArrEndIndex]){
-          this.sortIndex = (nums1.length + nums2.length) / 2 - nums1.length;
-          boolean firstArrIsLargerSecond = nums1[firstArrStartIndex]>nums2[secondArrEndIndex]?true : false;
-          int nextIndex = firstArrIsLargerSecond?1:-1;
-          return isOdd?Double.valueOf(this.getInSortIndex(sortIndex)):Double.valueOf(this.getInSortIndex(sortIndex,firstArrIsLargerSecond),this.getInSortIndex(sortIndex + nextIndex,firstArrIsLargerSecond))
-       		// if(firstArrIsLargerSecond){
-         //    this.sortIndex = (nums1.length + nums2.length) / 2 - nums2.length;
-       		// 	if(isOdd)
-       		// 		return sortIndex>0?nums1[sortIndex]:nums2[secondArrEndIndex + 1 + sortIndex];
-         //    else{
-         //      return sortIndex>0?Double.valueOf(nums1[sortIndex]+nums1[sortIndex-1])/2:Double.valueOf(nums2[secondArrEndIndex+1+sortIndex]+nums2[secondArrEndIndex+sortIndex])/2;
-         //    }
-       		// }else{
-         //    this.sortIndex = (nums1.length + nums2.length)/2 - nums1.length;
-         //    if(isOdd)
-         //      return sortIndex>0?nums2[sortIndex] : nums1[firstArrEndIndex + 1 + sortIndex];
-         //    else{
-         //      return sortIndex>0?Double.valueOf(nums2[sortIndex] + nums2[sortIndex-1])/2:
-         //              Double.valueOf(nums1[firstArrEndIndex+1+sortIndex]+nums1[firstArrStartIndex+sortIndex])/2;
-         //    }
-         //  }
+       	if(this.nums2[secondArrEndIndex]<this.nums1[firstArrStartIndex]||this.nums2[secondArrStartIndex]>this.nums1[firstArrEndIndex]){
+          this.sortIndex = (this.nums1.length + this.nums2.length) / 2 - this.nums1.length;
+          boolean firstArrIsLargerSecond = this.nums1[firstArrStartIndex]>this.nums2[secondArrEndIndex]?true : false;
+          int nextIndex = firstArrIsLargerSecond?-1:1;
+          return isOdd?Double.valueOf(this.getInSortIndex(sortIndex,firstArrIsLargerSecond)):Double.valueOf(this.getInSortIndex(sortIndex,firstArrIsLargerSecond) + this.getInSortIndex(sortIndex + nextIndex,firstArrIsLargerSecond))/2;
+       	
        	}else{
           this.find();
           return this.getResult();
@@ -49,29 +49,37 @@ public class MedianOfTwoSortedArray {
 
 
     }
-    //获取 sortIndex 所指示的数
+  //获取 sortIndex 所指示的数
     public int getInSortIndex(int sortIndex,boolean firstArrIsLargerSecond){
       if(firstArrIsLargerSecond)
-        return sortIndex>0?nums2[secondArrEndIndex - sortIndex]:nums1[firstArrStartIndex - 1 - sortIndex];
-      else
-        return sortIndex>0?nums2[sortIndex]:nums1[firstArrEndIndex+1+sortIndex];
+        return sortIndex>-1?nums2[secondArrEndIndex - sortIndex]:nums1[firstArrStartIndex - 1 - sortIndex];
+      else{
+        if(isOdd)
+          return sortIndex>-1?nums2[sortIndex]:nums1[firstArrEndIndex + sortIndex +1];
+        else
+          return sortIndex>0?nums2[sortIndex-1] : nums1[firstArrEndIndex+sortIndex];
+      }
+        // return sortIndex>0?nums2[sortIndex]:nums1[firstArrEndIndex+1+sortIndex];
     }
 
     public double getResult(){
       int firstNum = 0;
       int secondNum = 0;
-      while(sortIndex<(nums2.length + nums1.length)/2){
-        this.getNowLeast();
-        sortIndex++;
-        if(sortIndex == (nums1.length + nums2.length)/2){
-          firstNum = this.getNowLeast();
+      while(nowIndex<(nums2.length + nums1.length)/2){
+    	  firstNum = this.getNowLeast();
+    	  nowIndex++;
+        if(nowIndex == (nums1.length + nums2.length)/2){
+//          firstNum = this.getNowLeast();
           secondNum = this.getNowLeast();
         }
-        return isOdd ? Double.valueOf(firstNum) : (Double.valueOf(firstNum)+Double.valueOf(secondNum))/2;
+        
       }
+      return isOdd ? Double.valueOf(secondNum) : (Double.valueOf(firstNum)+Double.valueOf(secondNum))/2;
     }
 
     public int getNowLeast(){
+      if(firstArrStartIndex>firstArrEndIndex ||secondArrStartIndex>secondArrEndIndex)
+        return firstArrStartIndex>firstArrEndIndex?nums2[secondArrStartIndex++]:nums1[firstArrStartIndex++];
       if(nums1[firstArrStartIndex]>nums2[secondArrStartIndex])
         return nums2[secondArrStartIndex++];
       else
@@ -86,31 +94,50 @@ public class MedianOfTwoSortedArray {
     //   fields[minorMiddle] = ()
     // }
 
+    // public boolean find(){
+    //   if(nums1[firstArrMiddleIndex]>nums2[secondArrMiddleIndex]){
+    //     if(secondArrMiddleIndex - secondArrStartIndex + nowIndex < (nums1.length + nums2.length)/2){
+    //       this.nowIndex += secondArrMiddleIndex - secondArrStartIndex;
+    //       this.firstArrEndIndex = this.firstArrMiddleIndex + (this.firstArrStartIndex + this.firstArrEndIndex)%2 ;
+    //       this.firstArrMiddleIndex = (firstArrStartIndex + firstArrEndIndex)/2 ;//+ (firstArrStartIndex + firstArrEndIndex)%2 ;
+    //       this.secondArrStartIndex = this.secondArrMiddleIndex;
+    //       this.secondArrMiddleIndex = (secondArrStartIndex + secondArrEndIndex)/2 ;//+ (secondArrStartIndex + secondArrEndIndex)%2;
+    //       return this.firstArrMiddleIndex==this.firstArrStartIndex||this.secondArrStartIndex==this.secondArrMiddleIndex||find();//nowIndex<(nums1.length + nums2.length)/2||
+    //     }else{
+    //       return false;
+    //     }
+    //   }else{
+    //     if(nowIndex + firstArrMiddleIndex - firstArrStartIndex <(nums1.length + nums2.length)/2){
+    //       this.nowIndex += firstArrMiddleIndex - firstArrStartIndex;
+    //       this.firstArrStartIndex = this.firstArrMiddleIndex;
+    //       this.firstArrMiddleIndex = (this.firstArrStartIndex + this.firstArrEndIndex)/2;
+    //       this.secondArrEndIndex = this.secondArrMiddleIndex + (this.secondArrStartIndex+this.secondArrEndIndex)%2;
+    //       this.secondArrMiddleIndex = (this.secondArrStartIndex + this.secondArrEndIndex)/2;
+    //       return this.firstArrMiddleIndex==this.firstArrStartIndex||this.secondArrMiddleIndex==this.secondArrStartIndex||find();
+    //     }else{
+    //       return false;
+    //     }
+    //   }
+
+    // }
+
     public boolean find(){
       if(nums1[firstArrMiddleIndex]>nums2[secondArrMiddleIndex]){
-        if(secondArrMiddleIndex - secondArrStartIndex + nowIndex < (nums1.length + nums2.length)/2){
-          this.nowIndex += secondArrMiddleIndex - secondArrStartIndex;
-          this.firstArrEndIndex = this.firstArrMiddleIndex + 1;
-          this.firstArrMiddleIndex = (firstArrStartIndex + firstArrEndIndex)/2;
-          this.secondArrStartIndex = this.secondArrMiddleIndex;
-          this.secondArrMiddleIndex = (secondArrStartIndex + secondArrEndIndex)/2;
-          return this.firstArrMiddleIndex==this.firstArrStartIndex||this.secondArrMiddleIndex==this.secondArrStartIndex||find();//nowIndex<(nums1.length + nums2.length)/2||
-        }else{
-          return false;
-        }
+        int dropNum = this.firstArrEndIndex - (this.firstArrMiddleIndex + (this.firstArrStartIndex+this.firstArrEndIndex)%2);
+        nowIndex += dropNum;
+        this.secondArrStartIndex += dropNum;
+        this.secondArrMiddleIndex = (this.secondArrStartIndex + this.secondArrEndIndex)/2;
+        this.firstArrEndIndex = this.firstArrEndIndex - dropNum;
+        this.firstArrMiddleIndex = (this.firstArrStartIndex + this.firstArrEndIndex)/2;
+        return firstArrStartIndex==firstArrMiddleIndex||secondArrStartIndex==secondArrMiddleIndex||find();
       }else{
-        if(nowIndex + firstArrMiddleIndex - firstArrStartIndex <(nums1.length + nums2.length)/2){
-          this.nowIndex += firstArrMiddleIndex - firstArrStartIndex;
-          this.firstArrStartIndex = this.firstArrMiddleIndex;
-          this.firstArrMiddleIndex = (this.firstArrStartIndex + this.firstArrEndIndex)/2;
-          this.secondArrEndIndex = this.secondArrMiddleIndex + 1;
-          this.secondArrMiddleIndex = (this.secondArrStartIndex + this.secondArrEndIndex)/2;
-          return this.firstArrMiddleIndex==this.firstArrStartIndex||this.secondArrMiddleIndex==this.secondArrStartIndex||find();
-        }else{
-          return false;
-        }
+        int dropNum = this.firstArrMiddleIndex - this.firstArrStartIndex;
+        nowIndex += dropNum;
+        this.secondArrEndIndex -= dropNum;
+        this.secondArrMiddleIndex = (this.secondArrStartIndex+this.secondArrEndIndex)/2;
+        this.firstArrStartIndex = this.firstArrMiddleIndex;
+        this.firstArrMiddleIndex = (this.firstArrStartIndex+this.firstArrEndIndex)/2;
+        return firstArrStartIndex==firstArrMiddleIndex||secondArrStartIndex==secondArrMiddleIndex||find();
       }
-
     }
 }
-
